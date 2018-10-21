c0a43713fe3b39a371352ba29744932b109e2066
Luis Tejada <luistejadaa@macbook-pro-de-luis.local>
2018-10-21 07:27:54 -0400

    Initial Commit

---
+++ TestAppCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppCenter
+//
+//  Created by Luis Antonio Tejada Acevedo on 10/21/18.
+//  Copyright © 2018 Luis Tejada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
